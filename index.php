25fedff79072fec66e7293cd73f1df6672c487da
Salim Shrestha <26961315+salimshrestha98@users.noreply.github.com>
2023-02-24 20:16:10 +0545

    Add - Plugin Headers

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       Demo Plugin
+ * Plugin URI:        https://example.com/plugins/demo-plugin/
+ * Description:       A basic plugin built using ReactJS library.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            James Bond
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/demo-plugin/
+ * Text Domain:       demo-plugin
+ * Domain Path:       /languages
+ *
+ * @package Demo Plugin
+ */
